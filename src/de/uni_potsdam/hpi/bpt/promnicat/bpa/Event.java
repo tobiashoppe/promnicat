6c5a845e2decbdc9210fcfbefdd3a3b00163698b
rami.eidsabbagh <rami.eidsabbagh@FB4C25WS01.hpi.uni-potsdam.de>
2013-04-12 14:07:53 +0200

    - added

---
+++ src/de/uni_potsdam/hpi/bpt/promnicat/bpa/Event.java
@@ -0,0 +1,49 @@
+package de.uni_potsdam.hpi.bpt.promnicat.bpa;
+
+public abstract class Event {
+private int id;
+private String label;
+private int[] multiplicity;
+
+private void setID(int newid){
+	this.id = newid;
+}
+
+public int getID(){
+	return this.id;
+}
+
+public void setLabel(String newlabel){
+	this.label = newlabel;
+}
+
+
+public String getLabel(){
+	
+	return this.label;
+	
+}
+public void setMultiplicity(int[] mult){
+	this.multiplicity = mult;
+}
+
+
+public int[] getMultiplicity(){
+	return this.multiplicity;
+}
+
+public void getOwner(){ 
+	
+}
+
+private void setOwner(){
+	
+}
+
+public Event(int id, String label, int[] mult){
+	this.id = id;
+	this.label = label;
+	this.multiplicity = mult;
+}
+
+}
