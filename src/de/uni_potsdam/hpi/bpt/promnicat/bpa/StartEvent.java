29dd6ef4e6e24c7ba9e3415b2cf056116922ebae
rami.eidsabbagh <rami.eidsabbagh@hpi.uni-potsdam.de>
2013-04-24 18:06:05 +0200

    start event added

--- src/de/uni_potsdam/hpi/bpt/promnicat/bpa/StartEvent.java
+++ src/de/uni_potsdam/hpi/bpt/promnicat/bpa/StartEvent.java
@@ -9,11 +9,26 @@ package de.uni_potsdam.hpi.bpt.promnicat.bpa;
  */
 public class StartEvent extends ReceivingEvent {
 
+	private EventType type;
 	/**
-	 * 
+	 * @param eventid
+	 * @param bpid
+	 * @param label
+	 * @param mult
 	 */
-	public StartEvent() {
+	public StartEvent(int eventid, int bpid, String label, int[] mult) {
+		
+		super(eventid, bpid, label, mult);
+		type = EventType.STARTEVENT;
 		// TODO Auto-generated constructor stub
 	}
+	public EventType getType() {
+		return type;
+	}
+	public void setType(EventType type) {
+		this.type = type;
+	}
+	
 
+	
 }
